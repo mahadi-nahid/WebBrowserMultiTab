ec018dd0287f8301cd61dd9f6529401bfac5aa6d
Md. Mahadi Hasan Nahid <nahid@student.sust.edu>
2014-11-02 23:13:22 -0800

    Multi Tab Web Browser Project Created

---
+++ MultiTabWebBrowser/src/multitabwebbrowser/MultiTabWebBrowser.java
@@ -0,0 +1,17 @@
+
+package multitabwebbrowser;
+
+/**
+ *
+ * @author MdMahadiHasan
+ */
+public class MultiTabWebBrowser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
